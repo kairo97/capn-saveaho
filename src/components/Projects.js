9b3a03a1c9d08e2a558cbfd090d9df8495f14148
Kairo97 <109006341+kairo97@users.noreply.github.com>
2023-04-06 13:12:31 -0700

    got errors solved and rendered autoimages

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,22 +1,25 @@
 
 
-
-import "./Projects.css"
 import AutoProjects from "./AutoProjects";
+import "./Projects.css"
 import { useState } from "react";
 
 function Projects(){
      const [navItem, setNavItem] = useState('')
+    
+          
+     
 return(
      <div className="navBar">
           <ul className="navMenu">
-               <li className="navItem" onClick={setNavItem('auto')}>Auto Repair</li>
-               <li className="navItem" onClick={setNavItem('floor')}>Flooring</li>
-               <li className="navItem" onClick={setNavItem('drywall')}>Drywall</li>
-               <li className="navItem" onClick={setNavItem('misc')}>Misc</li>
+               <li className="navItem" id="auto" onClick={() => setNavItem("auto")}>Auto Repair</li>
+               <li className="navItem" id="floor" onClick={() => setNavItem('floor')}>Flooring</li>
+               <li className="navItem" id="drywall" onClick={() => setNavItem('drywall')}>Drywall</li>
+               <li className="navItem" id="misc" onClick={() => setNavItem('misc')}>Misc</li>
           </ul>
           <div className="projectContainer">
                {navItem === "auto" && <AutoProjects/>}
+               
           </div>
 
      </div>
