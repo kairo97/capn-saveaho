3ea3c2f48f8044d1af36c913c4d287b2b2efd9e3
Kairo97 <109006341+kairo97@users.noreply.github.com>
2023-04-09 10:38:23 -0700

    got reviews rendering to page, need to make look decent still

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import React, { useState } from "react"
 import "./Review.css"
 
 function Review() {
@@ -8,18 +8,28 @@ function Review() {
         id:0,
         text: "We hired Jesse for many different jobs, from framing, sheetrock, plumbing, and auto repair. He's always honest, fair, and hard working! (His sidekick, a super mellow well mannered black lab is a sweetheart too!) we highly recommend Jesse for just about any job around the home or vehicles. If he can't do it he'll connect you with someone who can. Jesse is a great guy you can really trust to get the job done!",
         author: "Kara Mckay"
+    }, {
+        id:1,
+        text: " review 2",
+        author:"another reviewer"
     }
     ]
+    const handleNextClick = () => {
+        setReviewP((reviewP + 1) % reviews.length);
+      };
 
     return(
         <div className="reviewBox">
-            <div>
-            {reviews >= 0 && (
-          <p className="reviewP">
-            {reviews.find((review) => review.id === reviewP).text}
-          </p>
-        )}
+            <div className="reviewBoxHeader">
+                <h2 className="reviewBoxTitle">Reviews</h2>
+                <button className="reviewBtn" onClick={handleNextClick}>Next Review</button>
             </div>
+            {reviews.length > 0 && reviews.find((review) => review.id === reviewP) && (
+            <p className="reviewP">
+                 {reviews.find((review) => review.id === reviewP).text}
+                 </p>
+                )}
+
         </div>
     )
 }
