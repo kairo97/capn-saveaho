05ecf62993ee3b2c05690bb8203e0440cb0f3462
Kairo97 <109006341+kairo97@users.noreply.github.com>
2023-04-13 13:16:57 -0700

    got nodemailer working with basic helloworld

--- backend/api/MailRoute.js
+++ backend/api/MailRoute.js
@@ -1,6 +1,4 @@
 
-
-"use strict";
 const nodemailer = require("nodemailer");
 
 // async..await is not allowed in global scope, must use a wrapper
@@ -11,30 +9,29 @@ async function main() {
 
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
-    port: 587,
-    secure: false, // true for 465, false for other ports
+    service: "hotmail",
     auth: {
-      user: testAccount.user, // generated ethereal user
-      pass: testAccount.pass, // generated ethereal password
+      user: "capn.saveaho@outlook.com", // generated ethereal user
+      pass: "Scheduling1!", // generated ethereal password
     },
   });
 
   // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: '"Capn-Saveaho" <capn-saveaho@gmail.com>', // sender address
+  let info = {
+    from: 'capn.saveaho@outlook.com', // sender address
     to: "kai.ropp@gmail.com", // list of receivers
     subject: "new appointment", // Subject line
     text: "Hello world?", // plain text body
-    html: "<b>Hello world?</b>", // html body
-  });
+  };
+  transporter.sendMail(info, function (err, info) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log("sent:" + info.response)
+  })
 
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 }
 
 main().catch(console.error);
